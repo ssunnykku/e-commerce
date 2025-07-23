1914df9608494020c297d206137c04e9b6961977
ssunnykku <108388578+ssunnykku@users.noreply.github.com>
2025-07-24 01:59:38 +0900

    feat: User 엔티티 table명 변경(users) 어노테이션 추가

--- src/main/java/kr/hhplus/be/server/user/domain/entity/User.java
+++ src/main/java/kr/hhplus/be/server/user/domain/entity/User.java
@@ -2,14 +2,16 @@ package kr.hhplus.be.server.user.domain.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
