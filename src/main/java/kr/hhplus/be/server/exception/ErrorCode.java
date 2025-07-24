37bae65def26420bec63772d4004a8d7cae98956
ssunnykku <108388578+ssunnykku@users.noreply.github.com>
2025-07-25 00:42:31 +0900

    feat: ErrorCode 추가 - 쿠폰 재고 및 사용, 상품 재고 관련 ErrorCode 추가

--- src/main/java/kr/hhplus/be/server/exception/ErrorCode.java
+++ src/main/java/kr/hhplus/be/server/exception/ErrorCode.java
@@ -13,14 +13,16 @@ public enum ErrorCode {
     INVALID_TYPE(HttpStatus.INTERNAL_SERVER_ERROR, "유효하지 않은 유형의 코드입니다."),
     INSUFFICIENT_POINT(HttpStatus.BAD_REQUEST, "잔액이 부족합니다."),
 
-
     COUPON_NOT_FOUND(HttpStatus.NOT_FOUND, "쿠폰을 찾을 수 없습니다."),
-    COUPON_OUT_OF_STOCK(HttpStatus.UNPROCESSABLE_ENTITY, "쿠폰 재고가 없습니다."),
+    COUPON_OUT_OF_STOCK(HttpStatus.BAD_REQUEST, "쿠폰 재고가 없습니다."),
     USER_ALREADY_HAS_COUPON(HttpStatus.BAD_REQUEST, "이미 발급받은 쿠폰입니다."),
-    EXPIRED_COUPON(HttpStatus.UNPROCESSABLE_ENTITY, "쿠폰이 만료되었습니다."),
-    ALREADY_USED(HttpStatus.UNPROCESSABLE_ENTITY, "이미 사용한 쿠폰입니다."),
+    EXPIRED_COUPON(HttpStatus.BAD_REQUEST, "쿠폰이 만료되었습니다."),
+    ALREADY_USED(HttpStatus.BAD_REQUEST, "이미 사용한 쿠폰입니다."),
 
-    PRODUCT_OUT_OF_STOCK(HttpStatus.UNPROCESSABLE_ENTITY, "상품 재고가 없습니다."),;
+    PRODUCT_OUT_OF_STOCK(HttpStatus.BAD_REQUEST, "상품 재고가 없습니다."),
+    NOT_FOUND_ENTITY(HttpStatus.NOT_FOUND, "정보를 찾을 수 없습니다.")
+
+    ;
 
     private final HttpStatus status;
     private final String message;
