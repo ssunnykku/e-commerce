e03c79835f6f8105f721f9af641aff1dde72d2f6
ssunnykku <108388578+ssunnykku@users.noreply.github.com>
2025-08-15 03:45:16 +0900

    refactor: 쿠폰 발급 - transactionTemplate 적용

--- src/main/java/kr/hhplus/be/server/coupon/application/useCase/CreateCouponUseCase.java
+++ src/main/java/kr/hhplus/be/server/coupon/application/useCase/CreateCouponUseCase.java
@@ -3,20 +3,71 @@ package kr.hhplus.be.server.coupon.application.useCase;
 import kr.hhplus.be.server.common.aop.DistributedLock;
 import kr.hhplus.be.server.coupon.application.dto.CouponRequest;
 import kr.hhplus.be.server.coupon.application.dto.CouponResponse;
-import kr.hhplus.be.server.coupon.application.service.CreateCouponService;
+import kr.hhplus.be.server.coupon.domain.entity.Coupon;
+import kr.hhplus.be.server.coupon.domain.entity.CouponType;
+import kr.hhplus.be.server.coupon.infra.repositpry.port.CouponRepository;
+import kr.hhplus.be.server.coupon.infra.repositpry.port.CouponTypeRepository;
+import kr.hhplus.be.server.user.infra.reposistory.port.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.time.LocalDate;
 
 @Service
 @RequiredArgsConstructor
 public class CreateCouponUseCase {
-    private final RedissonClient redissonClient;
-    private final CreateCouponService createCouponService;
+    private final TransactionTemplate transactionTemplate;
+
+    private final CouponTypeRepository couponTypeRepository;
+    private final CouponRepository couponRepository;
+    private final UserRepository userRepository;
 
     @DistributedLock(domain = "couponType", key = "#request.couponTypeId")
     public CouponResponse execute(CouponRequest request) {
-            return createCouponService.executeInTransaction(request);
-        }
+        return transactionTemplate.execute(status -> {
+            try {
+                return executeInTransaction(request);
+            } catch (Exception e) {
+                // 예외 발생 시 트랜잭션 롤백
+                status.setRollbackOnly();
+                throw e;
+            }
+        });
     }
+
+    @Transactional
+    public CouponResponse executeInTransaction(CouponRequest request) {
+        // 1. 쿠폰 재고 조회
+        userRepository.findById(request.userId());
+        CouponType couponType = findCouponType(request.couponTypeId());
+
+        // 2. 쿠폰 발급 대상 여부 확인 (중복 발급 불가)
+        checkUserHasNoCoupon(request.userId(), request.couponTypeId());
+
+        LocalDate expiresAt = couponType.calculateExpireDate();
+
+        // 3. 쿠폰 발급 처리
+        Coupon coupon = issueCouponToUser(couponType, request.userId());
+        coupon.setExpiresAt(expiresAt);
+        Coupon savedCoupon = couponRepository.save(coupon);
+
+        return CouponResponse.from(savedCoupon, couponType);
+    }
+
+    private CouponType findCouponType(Long couponTypeId) {
+        return couponTypeRepository.findById(couponTypeId);
+    }
+
+    private void checkUserHasNoCoupon(Long userId, Long couponTypeId) {
+        couponRepository.findByUserIdAndCouponTypeId(userId, couponTypeId);
+    }
+
+    private Coupon issueCouponToUser(CouponType couponType, Long userId) {
+        return couponType.issueTo(userId);
+    }
+
+
+}
 
