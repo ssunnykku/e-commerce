df039b8d6dc9359baba5660ba6a1b03b7809c160
ssunnykku <108388578+ssunnykku@users.noreply.github.com>
2025-09-12 07:22:41 +0900

    refactor: 상품리스트 조회 페이지네이션 추가 등 - 상품리스트 조회 페이지네이션 추가 - CouponStockService 하드코딩 제거 - 불필요 코드 제거 - application.yml ddl 설정 변경

--- src/main/java/kr/hhplus/be/server/product/application/useCase/GetProductListUseCase.java
+++ src/main/java/kr/hhplus/be/server/product/application/useCase/GetProductListUseCase.java
@@ -4,6 +4,8 @@ import kr.hhplus.be.server.product.application.dto.ProductResponse;
 import kr.hhplus.be.server.product.domain.entity.Product;
 import kr.hhplus.be.server.product.infra.repository.port.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,8 +16,16 @@ import java.util.stream.Collectors;
 public class GetProductListUseCase {
     private final ProductRepository productRepository;
 
+    public List<ProductResponse> execute(int page, int size) {
+       Page<Product> productList =  productRepository.findAll(PageRequest.of(page, size));
+
+        return productList.stream()
+                .map(product -> ProductResponse.from(product))
+                .collect(Collectors.toList());
+    }
+
     public List<ProductResponse> execute() {
-       List<Product> productList =  productRepository.findAll();
+        List<Product> productList =  productRepository.findAll();
 
         return productList.stream()
                 .map(product -> ProductResponse.from(product))
